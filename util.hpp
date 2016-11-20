4caac024993a4693e13b34d5141eeb2854874dc2
Ethan <ethanabrooks@gmail.com>
2016-11-20 01:49:24 -0500

    add files

---
+++ util.hpp
@@ -0,0 +1,7 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+
+#define range(i, start, end) for (i = start; i < end; i++)
+
+int idx2c(int i, int j, int width);
+void check(bool condition, const char *msg);
