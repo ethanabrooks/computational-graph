2cd98ee3bdb1dd9bdf0666f32c72e3b48e674f62
Ethan <ethanabrooks@gmail.com>
2016-11-20 01:30:59 -0500

    init

--- util.h
+++
@@ -1,7 +0,0 @@
-#include <stdio.h> 
-#include <stdlib.h> 
-
-#define range(i, start, end) for (i = start; i < end; i++)
-
-int idx2c(int i, int j, int width);
-void check(bool condition, const char *msg);
