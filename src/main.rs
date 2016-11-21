b01de0771a88b2050f3c4ababce2b0ff4707e42e
Ethan <ethanabrooks@gmail.com>
2016-11-21 13:54:33 -0500

    reference hell

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+mod graph; 
+mod constant; 
+use constant::Constant;
+use constant::Constant::scalar;
+
 fn main() {
-    println!("Hello, world!");
+	//let x: Constant = scalar { value: 2.0 };
+	//let y: Constant = scalar { value: 3.0 };
+    //println!("{:?}", constant::tanh(x + y));
+    println!("compiles");
 }
