67f4fac409125b59710a5468e14a7b4e5ce7bf01
Ethan <ethanabrooks@gmail.com>
2016-12-22 17:33:54 -0500

    looking good. not tested yet.

--- src/function/mod.rs
+++ src/function/mod.rs
@@ -2,7 +2,6 @@ pub use self::datatypes::{Function, Expr, Constant};
 
 mod constructors;
 mod datatypes;
-mod ops;
 mod print;
 mod optimize;
 
@@ -48,7 +47,8 @@ macro_rules! fn1 {
                     &Constant::Scalar(x) => Constant::Scalar($scalar_fn(x)),
                     &Constant::Matrix(ref m) => {
                         let mut result = Matrix::empty_like(m);
-                        unsafe { concat_idents!(map_, $op)(m, &mut result) };
+                        let matrix_fn = concat_idents!(map_, $op);
+                        unsafe { matrix_fn(m, &mut result) };
                         Constant::Matrix(result)
                     }
                 }
@@ -77,7 +77,7 @@ macro_rules! apply2 {
 }
 
 macro_rules! trait1 {
-    ($Op:ident, $op:ident, $idem:expr) => {
+    ($Op:ident, $op:ident, $idem:expr, $scalar_fn:expr) => {
         impl<'a> $Op for &'a Function {
             type Output = Function;
             fn $op(self) -> Function {
@@ -94,6 +94,25 @@ macro_rules! trait1 {
         impl $Op for Function {
             type Output = Function;
             fn  $op(self) -> Function { (&self).$op() }
+        }
+        impl $Op for Constant {
+            type Output = Constant;
+            fn $op(self) -> Constant { (&self).$op() }
+        }
+
+        impl<'a> $Op for &'a Constant {
+            type Output = Constant;
+            fn $op(self) -> Constant {
+                match self {
+                    &Constant::Scalar(x) => Constant::Scalar($scalar_fn(x)),
+                    &Constant::Matrix(ref m) => {
+                        let mut result = Matrix::empty_like(m);
+                        let matrix_fn = concat_idents!(map_, $op);
+                        unsafe { matrix_fn(m, &mut result) };
+                        Constant::Matrix(result)
+                    }
+                }
+            }
         }
     }
 }
@@ -120,6 +139,39 @@ macro_rules! trait2 {
             type Output = Function;
             fn  $op(self, other: Function) -> Function { (&self).$op(&other) }
         }
+
+        impl $Op for Constant {
+            type Output = Constant;
+            fn $op(self, other: Constant) -> Constant { (&self).$op(&other) }
+        }
+
+        impl<'a> $Op for &'a Constant {
+            type Output = Constant;
+            fn $op(self, other: &'a Constant) -> Constant {
+                match (self, other) {
+                    (&Constant::Scalar(x1), &Constant::Scalar(x2)) => {
+                        Constant::Scalar(x1.$op(x2)) }
+                    (&Constant::Scalar(x), &Constant::Matrix(ref m)) => {
+                        let mut result = Matrix::empty_like(m);
+                        let scalar_matrix_fun = concat_idents!(broadcast_, $op);
+                        unsafe { scalar_matrix_fun(x, m, &mut result) };
+                        Constant::Matrix(result)
+                    }
+                    (&Constant::Matrix(ref m), &Constant::Scalar(x)) => {
+                        let mut result = Matrix::empty_like(m);
+                        let matrix_scalar_fun = concat_idents!(broadcast_, $op, _rev);
+                        unsafe { matrix_scalar_fun(m, x, &mut result) };
+                        Constant::Matrix(result)
+                    }
+                    (&Constant::Matrix(ref m1), &Constant::Matrix(ref m2)) => {
+                        let mut result = Matrix::empty_like(m1);
+                        let matrix_fun = concat_idents!(elemwise_, $op);
+                        unsafe { matrix_fun(m1, m2, &mut result) };
+                        Constant::Matrix(result)
+                    }
+                }
+            }
+        }
     }
 }
 
@@ -128,11 +180,10 @@ fn1!(Tanh, tanh);
 fn1!(Signum, signum);
 fn1!(Sq, sq, |x: f32| x * x);
 fn1!(Sigmoid, sigmoid, |x: f32| 1. / (1. + (-x).exp()));
-trait1!(Neg, neg, 0.);
+trait1!(Neg, neg, 0., |x: f32| -x);
 trait2!(Add, add, 0.);
 trait2!(Sub, sub, 0.);
 trait2!(Mul, mul, 0.);
-
 
 impl Function {
     fn all_equal(&self, val:f32) -> bool {
@@ -141,8 +192,6 @@ impl Function {
             _                     => false
         }
     }
-
-    pub fn signum(&self) -> Function { signum(&self) }
 }
 
 pub fn dot_transpose(f1: &Function, f2: &Function, trans1: bool, trans2: bool) -> Function {
@@ -160,72 +209,20 @@ pub fn dot(f1: &Function, f2: &Function) -> Function {
     dot_transpose(f1, f2, false, false)
 }
 
-fn apply2(scalar_fun: &Fn(f32, f32) -> f32, 
-             scalar_matrix_fun: unsafe extern "C" fn(f32, *const Matrix, *mut Matrix),
-             matrix_scalar_fun: unsafe extern "C" fn(*const Matrix, f32, *mut Matrix),
-             matrix_fun: unsafe extern "C" fn(*const Matrix, *const Matrix, *mut Matrix),
-             c1: &Constant, c2: &Constant) -> Constant {
-    match (c1, c2) {
-        (&Constant::Scalar(x1), &Constant::Scalar(x2)) => {
-            Constant::Scalar(scalar_fun(x1, x2)) }
-        (&Constant::Scalar(x), &Constant::Matrix(ref m)) => {
-            let mut result = Matrix::empty_like(m);
-            unsafe { scalar_matrix_fun(x, m, &mut result) };
-            Constant::Matrix(result)
-        }
-        (&Constant::Matrix(ref m), &Constant::Scalar(x)) => {
-            let mut result = Matrix::empty_like(m);
-            unsafe { matrix_scalar_fun(m, x, &mut result) };
-            Constant::Matrix(result)
-        }
-        (&Constant::Matrix(ref m1), &Constant::Matrix(ref m2)) => {
-            let mut result = Matrix::empty_like(m1);
-            unsafe { matrix_fun(m1, m2, &mut result) };
-            Constant::Matrix(result)
-        }
-    }
-}
-
-// allocates on device
-fn apply2_comm(scalar_fun: &Fn(f32, f32) -> f32, 
-             broadcast_matrix_fun: unsafe extern "C" fn(f32, *const Matrix, *mut Matrix),
-             matrix_fun: unsafe extern "C" fn(*const Matrix, *const Matrix, *mut Matrix),
-             c1: &Constant, c2: &Constant) -> Constant {
-    let mut result;
-    match (c1, c2) {
-        (&Constant::Scalar(x1), &Constant::Scalar(x2)) => {
-            Constant::Scalar(scalar_fun(x1, x2)) }
-        (&Constant::Scalar(x), &Constant::Matrix(ref m)) |
-        (&Constant::Matrix(ref m), &Constant::Scalar(x)) => {
-            result = Matrix::empty_like(m);
-            unsafe { broadcast_matrix_fun(x, m, &mut result) };
-            Constant::Matrix(result)
-        }
-        (&Constant::Matrix(ref m1), &Constant::Matrix(ref m2)) => {
-            result = Matrix::empty_like(m1);
-            unsafe { matrix_fun(m1, m2, &mut result) };
-            Constant::Matrix(result)
-        }
-    }
-}
-
-fn sigmoid_f32(x: f32) -> f32 {
-    1. / (1. + (-x).exp())
-}
-
-
 impl Constant {
-    // allocates on device
-    fn apply(&self, scalar_fun: &Fn(f32) -> f32, 
-             matrix_fun: unsafe extern "C" fn(*const Matrix, *mut Matrix)
-             ) -> Constant {
+    pub fn avg(&self) -> f32 {
         match self {
-            &Constant::Scalar(x) => Constant::Scalar(scalar_fun(x)),
+            &Constant::Scalar(x) => x,
             &Constant::Matrix(ref m) => {
-                let mut result = Matrix::empty_like(m);
-                unsafe { matrix_fun(m, &mut result) };
-                Constant::Matrix(result)
+                (unsafe { reduce_sum(m) }) / m.size() as f32
             }
+        }
+    }
+
+    pub fn all_equal(&self, val: f32) -> bool {
+        match *self {
+            Constant::Scalar(x) => x == val,
+            Constant::Matrix(ref m) => unsafe { reduce_equal(m, val) },
         }
     }
 
@@ -249,30 +246,6 @@ impl Constant {
                 unsafe { matrix_fun(m1, m2, m1) },
             (&mut Constant::Scalar(ref mut x), &Constant::Matrix(_)) =>
                 scalar_fun(x, other.avg())
-        }
-    }
-
-    pub fn avg(&self) -> f32 {
-        match self {
-            &Constant::Scalar(x) => x,
-            &Constant::Matrix(ref m) => {
-                (unsafe { reduce_sum(m) }) / m.size() as f32
-            }
-        }
-    }
-
-    //pub fn sigmoid(&self) -> Constant {
-        //self.apply(&sigmoid_f32, map_sigmoid)
-    //}
-
-    //pub fn sq(&self) -> Constant {
-        //self.apply(&|x| x * x, map_sq)
-    //}
-
-    pub fn all_equal(&self, val: f32) -> bool {
-        match *self {
-            Constant::Scalar(x) => x == val,
-            Constant::Matrix(ref m) => unsafe { reduce_equal(m, val) },
         }
     }
 
@@ -308,72 +281,6 @@ impl Constant {
     }
 }
 
-// allocates on device
-impl Neg for Constant {
-    type Output = Constant;
-    fn neg(self) -> Constant { -&self }
-}
-
-// allocates on device
-impl Add for Constant {
-    type Output = Constant;
-    fn add(self, other: Constant) -> Constant { &self + &other }
-}
-
-// allocates on device
-impl Sub for Constant {
-    type Output = Constant;
-    fn sub(self, other: Constant) -> Constant { &self - &other }
-}
-
-// allocates on device
-impl Mul for Constant {
-    type Output = Constant;
-    fn mul(self, other: Constant) -> Constant { &self * &other }
-}
-
-// allocates on device
-impl<'a> Neg for &'a Constant {
-    type Output = Constant;
-    fn neg(self) -> Constant {
-        self.apply(&|x: f32| -x, map_neg)
-    }
-}
-
-// allocates on device
-impl<'a> Add for &'a Constant {
-    type Output = Constant;
-    fn add(self, other: &'a Constant) -> Constant {
-        apply2_comm(&|x1: f32, x2| x1 + x2,
-                       broadcast_add,
-                       elemwise_add,
-                       &self, &other)
-    }
-}
-
-// allocates on device
-impl<'a> Sub for &'a Constant {
-    type Output = Constant;
-    fn sub(self, other: &'a Constant) -> Constant {
-        apply2(&|x1: f32, x2| x1 - x2,
-                  broadcast_sub,
-                  broadcast_sub_rev,
-                  elemwise_sub,
-                  &self, &other)
-    }
-}
-
-// allocates on device
-impl<'a> Mul for &'a Constant {
-    type Output = Constant;
-    fn mul(self, other: &'a Constant) -> Constant { 
-        apply2_comm(&|x1, x2| x1 * x2,
-                       broadcast_mul,
-                       elemwise_mul,
-                       &self, &other)
-    }
-}
-
 impl SubAssign for Constant {
     fn sub_assign(&mut self, other: Constant) {
         self.assign2(&other, &|x1: &mut f32, x2| *x1 -= x2, 
@@ -404,7 +311,7 @@ pub fn sub_assign(c: &mut Constant, other: &Constant) {
 }
 
 pub fn sigmoid_assign(c: &mut Constant) {
-    c.assign1(&sigmoid_f32, map_sigmoid);
+    c.assign1(&|x| 1. / (1. + (-x).exp()), map_sigmoid);
 }
 
 pub fn tanh_assign(c: &mut Constant) {
