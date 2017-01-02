4dc2babfca4b9e63c7ece731b07d4a41fcac51d9
Ethan <ethanabrooks@gmail.com>
2017-01-01 20:54:09 -0500

    added files

---
+++ src/cpu/util.h
@@ -0,0 +1,18 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+#define rng(i, start, end) for(i = start; i < end; i++)
+void check(int condition, const char *msg);
+
+template<typename T> inline T* safe_malloc(int count) {
+    T *array = (T *)malloc(count * sizeof(*array));
+    check(!array, "safe_malloc failed"); 
+    return array;
+}
+
+//inline float* float_malloc(int count) {
+    //float *array = (float *)malloc(count * sizeof(*array));
+    //check(!array, "safe_malloc failed"); 
+    //return array;
+//}
+#endif
