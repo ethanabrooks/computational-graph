14ebb5d8bc616097352eaa8d09135bc2af79600f
Ethan <ethanabrooks@gmail.com>
2017-01-30 13:45:33 -0500

    tweaked build.rs

--- src/cpu/matrix.h
+++ src/cpu/matrix.h
@@ -12,14 +12,12 @@ extern "C" {
   int size(const Matrix *m);
   float* get_array(const Matrix *matrix);
   void set_array(Matrix *matrix, const float *array, bool transpose);
-  void init_cublas();
   void copy_matrix(const Matrix *src, Matrix *dst);
   void init_matrix(Matrix *matrix, const float *array,
           unsigned height, unsigned width);
   void fill_matrix(Matrix *matrix, float value);
   void print_matrix(const Matrix *matrix);
   void free_matrix(Matrix *matrix);
-  void maybe_init_cublas();
 }
 
 #endif
